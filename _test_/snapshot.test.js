efae5ae780c0d618e3e74bed722677a46b5fd14d
joel123vilca <joevil23tjq@gmail.com>
2019-01-04 23:07:25 -0500

    snapshot en jest

--- _test_/snapshot.test.js
+++ _test_/snapshot.test.js
@@ -1,8 +1,27 @@
 import { getCharacter } from '../snapshot';
-import Rick from '../rick';
+import rick from '../rick.json';
 
-describe('Es hora de la intentadas', () => {
-    test(Snapshot, () => {
-        expect(getCharacter(rick)).toMatchSnaps();
+describe('Es hora de las intantaneas', () => {
+    test('Snapshot', () => {
+        expect(getCharacter(rick)).toMatchSnapshot();
     });
+
+    test('Siempre fallara la instantanea', () => {
+        const user = {
+            createAt: new Date(),
+            id: Math.floor(Math.random() * 20),
+        }
+        expect(user).toMatchSnapshot();
+    });
+
+    test('Tenemos una excepsion dentro del codigo', () => {
+        const user = {
+            id: Math.floor(Math.random() * 20),
+            name: "Oscar Barajas Tavares"
+        }
+        expect(user).toMatchSnapshot({
+            id:  expect.any(Number)
+        });
+    });
+
 });
